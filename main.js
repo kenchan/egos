8e36eeec4c30605050b34be3ea18472bed3ee321
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2016-02-23 10:52:55 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var Twitter = require('twitter');
+var Request = require('request');
+
+var client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+client.stream('statuses/filter', {track: 'pbtech'}, function(stream) {
+  stream.on('data', function(tweet) {
+    Request.post(process.env.TAKOSAN_URL).form({
+      channel: process.env.SLACK_CHANNEL,
+      name: 'ついったーさん'
+      icon: ':twitter:',
+      author_icon: tweet.user.profile_image_url,
+      author_name: tweet.user.name,
+      author_link: 'https://twitter.com/' + tweet.user.screen_name,
+      text: tweet.text + "\n" + 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str
+    });
+    console.log(tweet.user.screen_name + ":" + tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
